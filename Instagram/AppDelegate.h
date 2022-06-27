e23b4c50162a50ce2b9f8aeefd3cc2e1665b3f55
Julia Navarro Goldaraz <julianavarrog@fb.com>
2022-06-27 13:07:19 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Julia Navarro Goldaraz on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
