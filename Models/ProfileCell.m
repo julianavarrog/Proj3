f3950cf495e2a45ad6fa97e019b5bff7e670d704
PP <46500171+ZHUANGPP@users.noreply.github.com>
2022-07-06 15:55:17 -0700

    Update ProfileCell.m

--- Models/ProfileCell.m
+++ Models/ProfileCell.m
@@ -18,12 +18,5 @@
     [self.profileImagePost loadInBackground];
     
 }
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
